015d79c37d1cf6ffe225c953249288c3f11cf123
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-04-25 16:09:04 -0500

    beginning logic & files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('McDonalds burger');
+INSERT INTO burgers (burger_name) VALUES ('Luger burger');
+INSERT INTO burgers (burger_name) VALUES ('Le Pigeon burger');
+INSERT INTO burgers (burger_name) VALUES ('Whiskey King burger');
+INSERT INTO burgers (burger_name) VALUES ('The Company burger');
+INSERT INTO burgers (burger_name) VALUES ('The Buckhorm burger');
